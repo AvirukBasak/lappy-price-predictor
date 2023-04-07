c7e062b02b31870ac217a037249073a09d12c5ec
AvirukBasak <avirukbasak@gmail.com>
2023-04-07 21:31:47 +0530

    changes to predict.py

--- modules/predict.py
+++ modules/predict.py
@@ -1,10 +1,32 @@
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
+import pandas as pd
+
 import pickle
+
+df_cleaned = pd.read_csv('data/csv/laptop_details_cleaned.csv')
+
+df_sel = pd.get_dummies(
+    df_cleaned,
+    columns=['os', 'cpu', 'ram_space', 'ram_type', 'storage_space', 'storage_type', 'touch_screen', 'warranty', 'display_size']
+)
+
+X_train, X_test, y_train, y_test = train_test_split(df_sel.drop('mrp', axis=1), df_sel['mrp'], test_size=0.2, random_state=42)
 
 model = None
 
-with open('models/lappy_price_predictor.pkl', 'rb') as file:
+'''
+model = LinearRegression()
+
+r2 = 0
+while r2 < 0.8:
+    model.fit(X_train, y_train)
+    r2 = model.score(X_test, y_test)
+
+print(f"R² = {r2:.4f}")
+'''
+
+with open('data/model/lappy_price_predictor.pkl', 'rb') as file:
     model = pickle.load(file)
 
 
@@ -30,4 +52,6 @@ def test():
         'warranty': '1 Y',
         'display_size': '13.0'
     }
+    r2 = model.score(X_test, y_test)
+    print(f"R² = {r2:.4f}")
     print(f"predicted price: {predict_price(features)}")
