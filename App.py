bc83151f9343615fc5ab5a8d86435e4c479f7060
AvirukBasak <avirukbasak@gmail.com>
2023-04-07 14:14:07 +0530

    init

---
+++ App.py
@@ -0,0 +1,7 @@
+import requests
+import streamlit as st
+
+st.set_page_config(
+    page_title='Laptop Price Predictor',
+    page_icon=''
+)
---
+++ README.md
@@ -0,0 +1,3 @@
+# Laptop Price Predictor
+Streamlit app to predict laptop prices based on multiple factors.
+
